a51ae482138a2724752839076019828d724ffc29
Shaurya <181509903+ShauryaSingh0-0@users.noreply.github.com>
2026-09-19 01:23:53 +0530

    Add files via upload

---
+++ Shopping_Cart.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    float total = 0.0f;
+    char currency = '$';
+    printf("Enter item name: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1]= '\0';
+    
+    printf("Enter price: ");
+    scanf("%f",&price);
+    
+    printf("Enter quantity: ");
+    scanf("%d",&quantity);
+    
+    total = quantity * price;
+    
+    printf("Your Total for %d %s ",quantity,item);
+    printf("is %c %.2f",currency,total);
+    return 0;
+}
